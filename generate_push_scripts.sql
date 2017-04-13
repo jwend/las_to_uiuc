6b0b2b411a0aca91f1674a158e2b56e01eec730d
Jeff Wendel <jwendel@usgs.gov>
2017-04-13 11:26:20 -0500

    use lftp scripting

--- generate_push_scripts.sql
+++ generate_push_scripts.sql
@@ -31,7 +31,24 @@ order by f.stock_id desc
 -- save the above result in a file and run it from unity.
 
 select 
-'mkdir -p z:/jwendel/lidar/projects/' ||regexp_replace(f.path, '\/[[:alnum:]|_|-]+\.zip$', '') || '; ' || 
+'mkdir -p z:/jwendel/lidar/projects/' ||regexp_replace(f.path, '/[^\/]+\.zip$', '') ||  chr(10) ||
+'cd z:/jwendel/lidar/projects/' ||regexp_replace(f.path, '/[^\/]+\.zip$', '') ||  chr(10) ||
+'lftp -c ''open suef.cr.usgs.gov && ' || 
+'cd /vdelivery/Datasets/Staged/Elevation/LPC/Projects/' || regexp_replace(f.path, '/[^\/]+\.zip$', '') || ' && ' || 
+'get ' || substr(regexp_substr(f.path, '/[^\/]+\.zip$'),2) || '''' || chr(10) ||
+'echo ' || f.path || '  >> z:/jwendel/lidar/projects.log'  || chr(10) 
+from las_project_files f 
+where f.path not in (select t.path from target_inventory t)
+and   f.path not in (select u.path from unity_inventory u)
+--and stock_id = 6389783
+order by f.stock_id desc
+
+
+-- save the above result and run it from a cygwin window on harmorny to pull from rockyftp
+
+
+select 
+'mkdir -p z:/jwendel/lidar/projects/' ||regexp_replace(f.path, '/[^\/]+\.zip$', '') || '; ' || 
 'cp x:/' || f.path || ' z:/jwendel/lidar/projects/' || f.path ||  '; ' || 
 'echo ' || f.stock_id || '  >> z:/jwendel/lidar/projects.log' 
 from las_project_files f 
@@ -39,4 +56,4 @@ where f.path not in (select t.path from target_inventory t)
 and   f.path not in (select u.path from unity_inventory u)
 order by f.stock_id desc
 
--- save the above result and run in from a cygwin window on harmony
+-- save the above result and run in from a cygwin window on harmony to pull from the netapp share
